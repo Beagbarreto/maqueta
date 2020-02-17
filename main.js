65a3f712af88ebc22f72489efe0a82ed81598799
lenbarreto <schambk@hotmail.com>
2020-02-17 00:21:23 -0600

    created general layout, row, columns and cards, added sample graph

---
+++ main.js
@@ -0,0 +1,16 @@
+const CHART = document.getElementById("myChart");
+
+let myChart = new Chart(CHART, {
+  type: 'line',
+
+// The data for our dataset
+data: {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [{
+        label: 'My First dataset',
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: [0, 10, 5, 2, 20, 30, 45]
+    }]
+},
+})
